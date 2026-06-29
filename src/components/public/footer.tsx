173ed112535fc2077eebe722cd68b0435b152c1a
Farid Erde <algoritmaerde@gmail.com>
2026-06-29 21:17:38 +0700

    footer: change background to black

--- src/components/public/footer.tsx
+++ src/components/public/footer.tsx
@@ -50,7 +50,7 @@ function FacebookIcon() {
 
 export function Footer() {
   return (
-    <footer className="border-t border-border bg-[oklch(0.18_0.02_160)] text-white">
+    <footer className="border-t border-border bg-black text-white">
       <Container className="py-12 sm:py-16">
         <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-4">
           {/* Brand */}
