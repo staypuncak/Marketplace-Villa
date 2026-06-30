eb4d4e17c0d534a3fdd6495749b0a945c470724c
Farid Erde <algoritmaerde@gmail.com>
2026-06-30 23:21:33 +0700

    fix: restructure headline into 3 lines

--- src/components/public/hero-section.tsx
+++ src/components/public/hero-section.tsx
@@ -132,7 +132,10 @@ export function HeroSection() {
         <Container className="relative z-10 flex min-h-[85vh] flex-col justify-center pb-20 pt-28">
           <div className="max-w-2xl">
             <h1 className="font-serif text-[1.375rem] leading-tight tracking-tighter text-white sm:text-5xl lg:text-6xl">
-              Liburan Berkesan di Puncak Lebih Hemat.{' '}
+              Liburan Berkesan di Puncak,
+              <br />
+              Lebih Hemat,
+              <br />
               <span className="text-amber-400">Tanpa Biaya Tambahan!</span>
             </h1>
             <p className="mt-4 max-w-xl text-lg text-white/90 sm:text-xl">
